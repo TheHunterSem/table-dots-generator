abfb4299a6d0fdfbaf822f3cfa421ae85982a5f3
Vasyl Kyryliuk <vasyl.kyryliuk@elogic.com.ua>
2018-06-06 15:50:04 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function ($) {
+
+    var addBtnRow = $('#add_row');
+    var removeBtnRow = $('#remove_row');
+
+    $(addBtnRow).on('click', function () {
+        $('.table-dots-wrapper .table-row:last').clone().appendTo('.table-dots-wrapper').find('.left-side .text,.right-side .text').text('');
+    });
+
+    $(removeBtnRow).on('click', function () {
+        if($('.table-dots-wrapper .table-row').length > 1) {
+            $('.table-dots-wrapper').find('.table-row:last').remove();
+        }
+    });
+
+   $('.table-dots-wrapper').on('click','.table-row .left-side, .table-row .right-side',function () {
+       var text = prompt("Input the text:", '');
+       if (text === null) {
+           var thisText = $('.text',this).text();
+           $('.text',this).text(thisText);
+       } else {
+           $('.text',this).text(text);
+       }
+   });
+
+    $('.table-dots-wrapper').on('click','.table-row .del',function () {
+        if($('.table-dots-wrapper .table-row').length > 1) {
+            $(this).parent().remove();
+        }
+    });
+
+});
